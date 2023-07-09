9a13a106e8954ce3f3f4b81a628d3e7eb1e4964e
rio <riovectored@gmail.com>
2023-07-09 09:52:04 +0700

    python interpreter diterjemahkan

--- Main.py
+++ Main.py
@@ -1,1 +1,4 @@
-print"hello world"`
+print("hello world")
+
+#python itu dari sourcecode main.py diterjemahkan python ke terminal untuk dijalankan
+#berbeda dengan cpp dengan compile dari source code ke compile lalu .exe dijalankan di terminal
