8375509f451dc31687816b71f42915878782ade7
rio <riovectored@gmail.com>
2023-07-10 09:26:17 +0700

    latihan variable

---
+++ variable.py
@@ -0,0 +1,20 @@
+#menaruh data di variable
+# var adalah tempat untuk menyimpan data
+#berbeda dengan c++ dan java yg ada deklarasi, pada python tidak ada contoh
+
+# int a contoh deklarasi
+
+b = 4
+a = (1 + 5) * b
+print("hasil ", a)
+
+#pemanggilan kedua
+
+a= 9
+print(a) 
+#nilai kedua a keluar
+
+b = a
+print("nilai a sekarang", a) 
+print("nilai b ", b)
+#yg dipakai adalah nilai a yg terakhir
