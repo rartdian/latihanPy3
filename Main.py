ab640bd98b5221b7179d838b3d6a54610241ba3c
rio <riovectored@gmail.com>
2023-07-10 08:59:46 +0700

    library time.time()

--- Main.py
+++ Main.py
@@ -1,4 +1,24 @@
+#menggunakan library waktu untuk menghitung lama program dijalankan
+import time #lib atau package time
+start_time = time.time()
+
+
 print("hello world")
 
 #python itu dari sourcecode main.py diterjemahkan python ke terminal untuk dijalankan
+#langsung di run tanpa harus compile,run atau compile python berdasarkan urutan atas ke bawah
+
 #berbeda dengan cpp dengan compile dari source code ke compile lalu .exe dijalankan di terminal
+
+a = 12 #assignment = menaruh ke memory
+print(a)
+
+#bisa compile python ke bytecode atau posisi sama dengan .exe dan lebih efisien 
+#dari source code ke python langsung ditampilkan
+
+
+#membuktikan compile lebih cepat daripada interpreted
+#cara compile Main.py   python -m py_compile Main.py
+
+#akhir pada time
+print(time.time() - start_time, "detik")
