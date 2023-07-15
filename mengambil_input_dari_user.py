0ed59f1705dbc048578516c82dd76884062d21f8
rio <riovectored@gmail.com>
2023-07-15 13:38:26 +0700

    data input normal adalah string, casting jika ingin tipe data lain

---
+++ mengambil_input_dari_user.py
@@ -0,0 +1,13 @@
+#input user
+nilai = input("masukkan nilai: ")
+print("nilainya adalah: ", nilai, type(nilai))
+#data yg diinput adalah string
+#jika yg diinginkan adalah data integer maka casting saja
+
+nilai_int = int(nilai)
+print(nilai_int, type(nilai_int))
+
+#atau
+
+data_number = float(input("masukkkan number desimal float : "))
+print(data_number, type(data_number))
