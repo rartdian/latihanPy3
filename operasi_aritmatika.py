b62b0ee1c577f92017d00f94376501b0ab75b0a3
rio <riovectored@gmail.com>
2023-07-15 15:05:32 +0700

    selain +-/* ada operator exponen atau dipangkatkan **

---
+++ operasi_aritmatika.py
@@ -0,0 +1,22 @@
+a = 100
+b = 40
+
+#perkalian
+hasil = a * b
+print(a ,"*", b ,"=", hasil, type(hasil))
+#pembagian
+hasil = a /b
+print(a, "/", b , "=", hasil, type(hasil)) #automatis float
+#pengurangan
+hasil = a - b
+print(a, "-", b, "=", hasil, type(hasil))
+#penjumlahan
+hasil = a + b
+print(a, "+", "b", "=", hasil, type(hasil))
+
+
+#ada operator yg tidak ada di bahasa lain yaitu exponen ** dipangkatkan
+a = 2
+b = 3
+hasil = a ** b
+print(hasil)
