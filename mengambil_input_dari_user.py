93ea5fd3d2c66d80007b9bffaa7bedeee495e318
rio <riovectored@gmail.com>
2023-07-15 14:37:12 +0700

    casting inptu data dari str ke int ke bool supaya bisa false

--- mengambil_input_dari_user.py
+++ mengambil_input_dari_user.py
@@ -11,3 +11,14 @@ print(nilai_int, type(nilai_int))
 
 data_number = float(input("masukkkan number desimal float : "))
 print(data_number, type(data_number))
+
+#boolean
+data_bool = bool(input("masukkan T/F: "))
+print(data_bool, type(data_bool)) #karena boolean bernilai false jika kosong,
+#maka input kita casting ke int terlebih dahulu lalu casting ke boolean.
+
+#contoh
+
+input_int = int(input("masukkan 1/0: "))
+data_bool = bool(input_int)
+print(data_bool, type(data_bool))
