9aaec057d0828ac84eadc7324e14a5aa6ee66fe4
rio <riovectored@gmail.com>
2023-07-16 13:46:19 +0700

    app konversi temp

---
+++ latihan-konversi-temp.py
@@ -0,0 +1,15 @@
+print("program konversi temperatur")
+celcius = float(input("masukkan suhu dalam celcius"))
+print("suhu celcius = ", celcius, "celcius")
+
+#reamur 
+reamur = (4/5)* celcius
+print("suhu dalam reamure = ",reamur,"reamur")
+
+#fahrenheit
+fahrenheit = (9/5 * celcius) +32
+print("suhu dalam fahrenheit = ",fahrenheit, "fahrenheit")
+
+#kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin = ",kelvin,"kelvin")
