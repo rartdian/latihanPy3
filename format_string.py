bd1e8e330cd6601fac9b8e5e03bf672dac6dc46b
rio <riovectored@gmail.com>
2023-08-05 07:11:27 +0700

    format string, bool,number,bil bulat, bilangan ribuan, bil desimal dll

--- format_string.py
+++ format_string.py
@@ -26,4 +26,44 @@ number = 9000000000
 format_str = f"juta = Rp. {number:,},-"
 print(format_str)
 
-#bil desim
+#bil desimal
+number = 20.543299999
+format_str = f"desimal = {number:.3f}" #ambil 3 angka dibelakang koma f=float
+print(format_str)
+
+#menampilkan leading zero
+number = 2101.290920
+format_str = f"desimal = {number:010.3f}"
+print(format_str)
+
+#menampilkan tanda + atau -
+nilai_minus = -10
+nilai_plus = +10.2323
+format_minus = f"minus = {nilai_minus:+d}"
+format_plus = f"plus = {nilai_plus:+.2f}"
+print(format_minus)
+print(format_plus)
+
+#format persen
+presentase = 0.045
+format_persen = f"presen = {presentase:.2%}"
+print(format_persen)
+
+
+
+#melakukan operasi aritmatika di dalam placeholder
+harga = 100000
+jumlah = 5
+format_string = f"harga total = Rp. {harga*jumlah:,}"
+print(format_string)
+
+#format angka lain (binary, octal, hexadecimal)
+
+nilai = 128
+format_binary = f"binary = {bin(nilai)}"
+format_octal = f"octal = {oct(nilai)}"
+format_hex = f"hex = {hex(nilai)}"
+
+print(format_binary)
+print(format_octal)
+print(format_hex)
