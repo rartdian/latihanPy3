4f4f72a3b94e7b88582f4861ec4350282078ab07
rio <riovectored@gmail.com>
2023-08-07 13:58:57 +0700

    latihan program kalender

--- latihan_date_time.py
+++ latihan_date_time.py
@@ -13,8 +13,14 @@ print(f"Hari ini tanggal: {hari_sekarang}")
 umur_day = hari_sekarang - tanggal_lahir
 umur_year = umur_day.days // 365
 umur_month_sisa = (umur_day.days % 365 ) // 30
+umur_day_sisa = (umur_day.days - tanggal_lahir)
+
+print(f"hari nya adalah: {tanggal_lahir:%A}")
+print(f"umur anda adalah: {umur_year} tahun,{umur_month_sisa} bulan, {umur_day_sisa} hari")
+
 #perbedaan mod dg floor division?
 a = 5
 b = 2
 floordiv = a // b #seharusnya 2.5 namun di cut dengan fungsi floor division jadi hasilnya adalah 2
 print(floordiv)
+
