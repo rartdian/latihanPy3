989d0992b4d36e660c17fd203494c4771bb68f38
rio <riovectored@gmail.com>
2023-10-20 07:06:46 +0700

    menambahkan tuples dan set

---
+++ tuples_set.py
@@ -0,0 +1,21 @@
+#list
+#menggunakan kurung siku
+data_list = [
+    2,3,3,1,23,4
+]
+print(data_list)
+
+#tuples
+data_tuples = (7,4,5,6,)
+print(data_tuples)
+print(data_tuples[1])
+
+#contoh yg tidak bisa dilakukan
+#data_tuples[1] = "rio"
+#data_tuples.append(1)
+
+#sets (himpunan)
+data_sets = {1,2,3,4,5}
+print(data_sets)
+
+# print(data_sets[0]) di himpunan tidak ada index sehingga jika dipanggil index tidk bisa, karena himpunan hanya bisa untuk memasukkan tidak untuk di rubah hanya dihitung sering digunakan statistik
